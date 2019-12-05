ee5d5d0ae8b6040918a68096937a5e7e215b2daf
amichalo421 <andre.michalowski@gmail.com>
2019-12-05 13:34:58 +0100

    Dashboard base component and functionality checklist

---
+++ my-app/src/Dashboard/Dashboard.js
@@ -0,0 +1,30 @@
+// ### Count Rules
+
+// [] - balls and strikes reset to 0 when a player reaches 3 strikes or 4 balls.
+// [] - balls and strikes reset to 0 when a `hit` is recorded.
+// [] - a `foul` increases strikes up to 2. With no strikes, a foul makes it 1 strike. With 1 strike, a foul makes it 2 strikes. With two strikes a foul has no effect, count stays at 2 strikes.
+
+// ### Display
+
+// [] - display the count of `balls` and `strikes` for the at-bat.
+// [] - should be updated when the user records activity on the `Dashboard` component.
+
+### Dashboard
+
+[x] - provide a button that the person in charge can press every time there is a `strike`, `ball`, `foul` or `hit`.
+// [x] - there is **NO** need to specify the type of hit (single, double, etc).
+[x] - changes recorded on this component should update the information shown by the `Display` component.
+// [] - Feel free add other components and organize and name your components any way you want to satisfy the requirements. **Make it up and make it happen developer!**.
+
+[] - 4 Buttons 
+    [] - strike
+    [] - ball
+    [] - foul
+    [] - hit
+
+[] - onclick state change to update display
+
+[] - relevant imports
+    [] - React
+    [] - useState/useEffect 
+    [] - Components
